ba872849686cfea32d1e38b3a74581d6865291c8
LiubovMavlieva <mavlieva13@icloud.com>
2021-10-17 00:37:28 +0300

    Ожидает первого ревью

--- webpack.config.js
+++ webpack.config.js
@@ -39,10 +39,8 @@ module.exports = {
         },
       },
       {
-        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
+
         use: [
           MiniCssExtractPlugin.loader,
           {
